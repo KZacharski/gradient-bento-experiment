7bd8d512523958fd191f85aaae737cf0f1cade55
KZacharski <puffercatt@gmail.com>
2023-11-28 01:44:47 +0100

    Initially prepared the first card

--- src/components/Desktop.vue
+++ src/components/Desktop.vue
@@ -9,12 +9,18 @@ defineProps({
 
 <template>
   <div class="p-3 w-2/3 h-full">
-  <div class="card w-full h-full bg-gradient-to-b from-white to-green-100 ring-1 ring-inset ring-black ring-opacity-10 relative">
-    <div class="flex flex-row h-fit absolute bottom-0 pb-10 pl-10">
-      <h3>Test</h3>
-    
+  <div class="card w-full h-full bg-gradient-to-b from-white to-green-100 ring-1 ring-inset ring-black ring-opacity-10 relative overflow-hidden">
+    <div class="flex flex-row h-full w-full">
+    <div class="flex flex-row w-full items-center justify-center">
+      <div class="flex flex-col pl-5 pr-5">
+      <h2 class="">Desktop window</h2>
+      <p class="">Idk some description lorem ipsum whatever</p>
+      </div>
     </div>
-    <img src="../assets/desktop.svg" class="absolute image-card bottom-0 right-0">
+    <div class="flex justify-end content-end h-full">
+    <img src="../assets/desktop.svg" class="image-card align-end justify-end self-end justify-self-end">
+    </div>
+    </div>
   </div>
   </div>
 </template>
@@ -25,10 +31,18 @@ h1 {
   font-size: 2.6rem;
   position: relative;
   top: -10px;
+} p {
+  font-size: large;
+  font-weight: 500;
+  width:;
 }
 
 h3 {
   font-size: 1.2rem;
+} h2 {
+  font-size: xx-large;
+  font-weight: 700;
+  width:;
 }
 
 .greetings h1,
@@ -42,8 +56,8 @@ h3 {
     text-align: left;
   }
 } .card {
-  border-radius: 30px;
-} .image-card {
-  border-bottom-right-radius: 30px;
+  border-radius: 30px; 
+}  .image-card {
+  width: 40rem;
 }
 </style>
